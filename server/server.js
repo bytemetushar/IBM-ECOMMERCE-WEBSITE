3725b6504e74435db4bc52b5c485be863730dce9
udsingh2024 <usamrat2004@gmail.com>
2026-07-20 22:06:58 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -2,16 +2,17 @@ import app from "./app.js";
 import connectionToDB from './config/dbConnection.js';
 
 const PORT = process.env.PORT || 5000;
-const startServer = async ()=>{
-    try{
+
+const startServer = async () => {
+    try {
         await connectionToDB();
-        app.listen(PORT, '127.0.0.1', ()=>{
-            console.log(`Server is listening at http://127.0.0.1:${PORT}...`)
-        })
-    }catch(error){
-        console.log("Failed to connect database, Try again...");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}...`);
+        });
+    } catch (error) {
+        console.log("Failed to connect database, Try again...", error);
         process.exit(1);
     }
-}
+};
 
 startServer();
